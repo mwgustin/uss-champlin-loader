97e8210db04b46a8a409f7e603df5b7436360eab
Mike Gustin <gustin.mike@gmail.com>
2020-07-03 13:40:36 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace uss_champlin_loader
+{
+    class Program
+    {
+        static ILogger _logger;
+        static void Main(string[] args)
+        {
+            //setup logger
+            var fac = LoggerFactory.Create(builder => 
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole();
+            });
+            
+            _logger = fac.CreateLogger<Program>();
+            
+            //startup
+            _logger.LogCritical("Beginning");
+            Thread.Sleep(2000);
+        }
+    }
+}
